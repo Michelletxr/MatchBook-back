385e2b3fc307d54fbd308f51e71dda782d711a1f
Michelle <michelle.teixeira.124@ufrn.edu.br>
2022-10-24 13:17:38 -0300

    feat: resolvendo bug na permissão

--- MatchBookBack/apps/book/permissions.py
+++ MatchBookBack/apps/book/permissions.py
@@ -9,11 +9,10 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     """
 
     def has_object_permission(self, request, view, obj):
-        print("objeto", request.user)
         # Read permissions are allowed to any request,
         # so we'll always allow GET, HEAD or OPTIONS requests.
         if request.method in permissions.SAFE_METHODS:
             return True
 
         # Write permissions are only allowed to the owner of the snippet.
-        return obj == request.user
+        return obj.user == request.user
