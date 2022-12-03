8a2fab643553d466ba6479df1e76b7a633057867
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2022-12-03 17:08:08 -0300

    feat: set static files

--- MatchBookBack/settings.py
+++ MatchBookBack/settings.py
@@ -144,6 +144,8 @@ USE_TZ = True
 
 STATIC_URL = 'staticfiles/'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
