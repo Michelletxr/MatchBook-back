a15f86867449f9e339ac2080e097bc3277d3389d
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2022-12-04 18:45:58 -0300

    feat: add pagination

--- MatchBookBack/settings.py
+++ MatchBookBack/settings.py
@@ -117,8 +117,8 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    # 'PAGE_SIZE': 10,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 SWAGGER_SETTINGS = {
